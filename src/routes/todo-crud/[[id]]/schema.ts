056d14c889adbdde859d49e4beae600c32b35150
Javi-cba <cordobajava@gmail.com>
2025-01-14 20:21:50 -0300

    JCJC

--- src/routes/todo-crud/[[id]]/schema.ts
+++ src/routes/todo-crud/[[id]]/schema.ts
@@ -1,9 +1,10 @@
+// ESQUEMA PARA VALIDACIÓN
 import { z } from 'zod';
 
 export const formSchema = z.object({
   id: z.string().optional(),
-  title: z.string().min(2).max(50),
-  description: z.string().min(2).max(50),
+  title: z.string().min(2).max(100),
+  description: z.string().min(2).max(500),
 });
 
 export type FormSchema = typeof formSchema;
